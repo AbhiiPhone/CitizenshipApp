4cdbcb5a5de64e7e13b8b0f336578eb701466c53
Romio <romio.micronix@gmail.com>
2017-12-26 19:35:11 +0530

    qwqwqw

--- Citizenship/AppDelegate.swift
+++ Citizenship/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // again test
     
-    // from romio
+    // from romio xxxx
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
