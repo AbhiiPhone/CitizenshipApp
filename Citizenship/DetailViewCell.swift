bc73cc9470a65ef4fcc7f66562fb64acb503fc3a
Romio <romio.micronix@gmail.com>
2017-12-27 11:18:56 +0530

    new

---
+++ Citizenship/DetailViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewCell.swift
+//  Citizenship
+//
+//  Created by Amit Kumar Poreli on 27/12/17.
+//  Copyright © 2017 Basir. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
